44cc38a0a54bbd23a4a4f4409b55be20d17f9d26
Mariano Dorda <mariano.dorda@ecloudsolutions.com>
2020-11-06 09:08:43 -0300

    creados los atributos base de cada tabla

---
+++ models/base.model.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+
+module.exports = {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	createdAt: {
+		type: Sequelize.DATE,
+		field: 'created_at',
+		defaultValue: Sequelize.NOW,
+		allowNull: false
+	},
+	updatedAt: {
+		type: Sequelize.DATE,
+		field: 'updated_at',
+		defaultValue: Sequelize.NOW,
+		allowNull: false
+	},
+	isDeleted: {
+		type: Sequelize.BOOLEAN,
+		field: 'is_deleted',
+		defaultValue: false,
+		allowNull: false
+	}
+}
